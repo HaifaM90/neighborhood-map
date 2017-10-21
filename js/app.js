68e15484175b36ff63b8b1f59b9defe9b6820f44
Haifa Alajlan <haifa@tamkeentech.sa>
2017-10-21 11:41:45 +0300

    map

--- js/app.js
+++ js/app.js
@@ -8,21 +8,28 @@ function viewModel  (){
 //filter function
   this.filterdLocations = ko.computed(function () {
     var filter = this.filter().toLowerCase();
+
     //if no filter valure return all
     if (!filter) {
+      // ko.utils.arrayForEach(self.locations(), function(location) {
+      //   addMarker(location);
+      // },this);
       return self.locations();
     }
     else {
       //compare filter input with location list
       return ko.utils.arrayFilter(self.locations(),function (location) {
         if(location.name.toLowerCase().indexOf(filter) !== -1){
+          addMarker(location);
         return location;
       }
       })
 
     }
 
+
   }, this);
+
   //click for more info.
 this.moreInfo= ko.computed( function(){
 ///get info from ajax
@@ -31,18 +38,6 @@ this.moreInfo= ko.computed( function(){
 
 });
 
-//
-//addMarkers(this.filterdLocations());
-// this.pins = ko.computed( function(){
-//   if(!this.filter) {
-//     addMarkers(self.locations());
-//   }
-//   else {
-//     addMarkers(self.filterdLocations());
-//   }
-//
-//
-// });
 }
 
 ko.applyBindings( new viewModel());
--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 var map;
 var uluru ;
-var marker ;
+var markers =[] ;
 var initMap =function () {
 uluru = {lat: 24.7366, lng: 46.6803};
 map = new google.maps.Map(document.getElementById('map'), {
@@ -8,15 +8,31 @@ map = new google.maps.Map(document.getElementById('map'), {
           center: uluru
         });
 
-//addMarkers(markers);
+
+
 }
- var addMarkers = function (places) {
-   places.forEach(function(place) {
-     marker= new google.maps.Marker({
+ var addMarker = function (place) {
+
+     var length = markers.push(new google.maps.Marker({
      position:place.cords,
      map: map
    }
- );
-});
+ ));
+ markers[length-1].addListener('click', function() {
+   console.log(this.getPosition());
+   this.setAnimation(google.maps.Animation.BOUNCE);
+   //getInformation from ajex
+   new google.maps.InfoWindow({
+          content: this.getPosition() + " "
+        }).open(map, this);
+ });
+}
 
- }
+
+
+// var clearMarkers = function() {
+//   for (var i = 0; i < markers.length; i++) {
+//             markers[i].setMap(null);
+//         }
+//     markers = [];
+// }
