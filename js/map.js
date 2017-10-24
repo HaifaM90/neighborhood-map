177b6f3bc1040aa7274ac420f0187f4f4bc5348b
Haifa <hma@Haifas-MacBook-Pro.local>
2017-10-24 23:34:53 +0300

    drop

--- js/map.js
+++ js/map.js
@@ -1,54 +1,53 @@
 var map;
 var uluru ;
 var initMap =function () {
-uluru = {lat: 24.7366, lng: 46.6803};
-map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 13,
-          center: uluru
-        });
+  uluru = {lat: 24.7366, lng: 46.6803};
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: uluru
+  });
 
- places =places.forEach(function(place) {
-  addMarker(place);
-});
+  places =places.forEach(function(place) {
+    addMarker(place);
+  });
 
 };
- var addMarker = function (place) {
+var addMarker = function (place) {
 
-     var marker = new google.maps.Marker({
-     position:place.cords,
-     map: map
-   }
- );
+  var marker = new google.maps.Marker({
+    position:place.cords,
+    map: map
+  }
+);
 
 marker.addListener('click', function() {
 
-     this.setAnimation(google.maps.Animation.BOUNCE);
+  this.setAnimation(google.maps.Animation.DROP);
 
-   getMoreInfo(place.id , this);
+    getMoreInfo(place.id , this);
 
- });
-
- place.marker = marker;
+  });
+  place.marker = marker;
 };
 
 var getMoreInfo = function (id, marker) {
   $.ajax('https://api.foursquare.com/v2/venues/'+id+'?v=20170101&client_id='+clientId+'&client_secret='+clientSecret,
   {
-        success: function(data) {
-        var info=  data.response.venue;
-          $('.message').html(' ');
-        new google.maps.InfoWindow({
-               content: '<h4>'+info.name+'</h4> '+'<strong>Categories:</strong> '+ info.categories[0].name+'<br><strong>Rating:</strong> '
-                + (info.rating?info.rating: ' ')
-             }).open(map, marker);
+    success: function(data) {
+      var info=  data.response.venue;
+      $('.message').html(' ');
+      new google.maps.InfoWindow({
+        content: '<h4>'+info.name+'</h4> '+'<strong>Categories:</strong> '+ info.categories[0].name+'<br><strong>Rating:</strong> '
+        + (info.rating?info.rating: ' ')
+      }).open(map, marker);
 
-        },
-        error: function() {
-          $('.message').html('There is somthing wents wrong try again later');
-        }
-     });
-   };
+    },
+    error: function() {
+      $('.message').html('There is somthing wents wrong try again later');
+    }
+  });
+};
 
 var fallBack = function() {
-       $('.message').html('There is somthing wents wrong try again later');
-   };
+  $('.message').html('There is somthing wents wrong try again later');
+};
